93195ac84a7a3381d705b9104f32d8914b2c42e2
Mettfan <cesaryannick6@gmail.com>
2022-08-02 21:09:29 -0500

    Nav Bar Fixed using class active

--- client/src/Components/Nav/Nav.jsx
+++ client/src/Components/Nav/Nav.jsx
@@ -4,7 +4,7 @@ import Menu from '../../Assets/Menu.png'
 import { useSelector } from "react-redux/es/hooks/useSelector";
 import { useState } from "react";
 import Cookie from 'universal-cookie'
-import { useNavigate } from 'react-router-dom'
+import { NavLink, useNavigate } from 'react-router-dom'
 let cookie = new Cookie()
 export default function Nav (){
     let [state, setState] = useState({
@@ -17,11 +17,13 @@ export default function Nav (){
         <div className="nav">
             <img src={Menu} className='menuPng'/>
 
-            { productos && <input type={'text'} placeholder='Buscar el producto...'></input>}
-             <button className="changePage" onClick={() => { nav('/home')}}>HOME</button>
+            {/* { productos && <input type={'text'} placeholder='Buscar el producto...'></input>} */}
+             {/* <button className="changePage" onClick={() => { nav('/home')}}>HOME</button>
              <button className="changePage" onClick={() => { nav('/catalog')}}> CATALOG </button>
-             <button className="changePage" onClick={() => { nav('/update/price')}}>SEARCH</button>
-
+             <button className="changePage" onClick={() => { nav('/update/price')}}>SEARCH</button> */}
+            <NavLink className= { 'changePage' } to={'/home'}><div className="navText">HOME</div></NavLink>
+            <NavLink className={ 'changePage' } to={'/catalog'}><div className="navText">CATALOG</div></NavLink>
+            <NavLink className={ 'changePage' } to={'/update/price'}><div className="navText">SEARCH</div></NavLink>
         </div>
     </>)
 
