412b11602599644bd81243165a28e5e68d889245
Mettfan <cesaryannick6@gmail.com>
2022-08-10 16:12:40 -0500

    Fixing Ticket

--- client/src/Components/UpdatePrice/PrintTest.js
+++ client/src/Components/UpdatePrice/PrintTest.js
@@ -4,19 +4,32 @@ import Mickey from '../../Assets/Mickey.png'
 import './PrintTest.css'
 
 export const Example = (propsRoot) => {
+  const date = new Date()
+  const currentDate = `${date.getDate()}/${date.getMonth()+1}/${date.getFullYear()}`;
+  const currentDateTime = date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds();;
   const ComponentToPrint = React.forwardRef((props, ref) => {
       return (
         <div ref={ref} className= 'mainTicketContainer' >
           <div>
-            Tiendas Don May
+            
+            <div className="ticketTitle" >Tiendas Don May</div>
+            <div>R.F.C.: TOCM520906 </div>
+            <div>{currentDate}</div>
+            <div>{currentDateTime}</div>
+            <div>San Antonio la Isla</div>
+            <div>Melchor Ocampo #301</div>
+            <div>C.P.: 52280</div>
+            <div>Tel: 722 371 1324</div>
             <img className="mickeyTicket" src={Mickey}/>
           </div>
+          <div className="divisorContainerStart"></div>
           <div>
             {propsRoot.ticket}
           </div>
+          <div className="divisorContainerEnd"></div>
           <div>
-            Calle Melchor Ocampo #301
-            <div>donmai.com</div>
+
+            <div>donmay.com</div>
           </div>
         </div>
       );
