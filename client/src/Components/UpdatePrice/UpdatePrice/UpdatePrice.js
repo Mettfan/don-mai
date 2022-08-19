3668eac9c2b7d8dc7c834456431bdbb496cb2db7
Mettfan <cesaryannick6@gmail.com>
2022-08-18 23:55:40 -0500

    Agregando visor de actualizacion de mes

--- client/src/Components/UpdatePrice/UpdatePrice/UpdatePrice.js
+++ client/src/Components/UpdatePrice/UpdatePrice/UpdatePrice.js
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { counterDecrement, counterIncrement, editOneProduct, fetchOneProduct } from "../../../features/products/productSlicetest";
 
 export default function UpdatePrice(){
+    let todaysDate = new Date()
     let [state, setState] = useState({
         idInput: null,
         nameInput: null,
@@ -67,25 +68,40 @@ export default function UpdatePrice(){
             </button>
             <div>
                 {selectedProduct.Producto}
+                <form name="Producto" onSubmit={(e)=> {handleOnEdit(e, 'Producto')}}>
+                    <input placeholder="Nuevo Nombre" name="Producto" type={'text'} onChange={(e) => {handleInputOnChange(e)}} />
+                </form>
                 
             </div>
-            <form name="Producto" onSubmit={(e)=> {handleOnEdit(e, 'Producto')}}>
-                <input placeholder="Nuevo Nombre" name="Producto" type={'text'} onChange={(e) => {handleInputOnChange(e)}} />
-            </form>
             <div>
                 {selectedProduct['P. Venta']}
+                <form name="P. Venta" onSubmit={(e)=> {handleOnEdit(e, 'P. Venta')}}>
+                    <input placeholder="Nuevo Precio" name="P. Venta" type={'number'} onChange={(e)=> handleInputOnChange(e)} />
+                </form>
 
             </div>
-            <form name="P. Venta" onSubmit={(e)=> {handleOnEdit(e, 'P. Venta')}}>
-                <input placeholder="Nuevo Precio" name="P. Venta" type={'number'} onChange={(e)=> handleInputOnChange(e)} />
-            </form>
             <div>
                 {selectedProduct['Departamento']}
+                <form name="Departamento" onSubmit={(e)=> {handleOnEdit(e, 'Departamento')}}>
+                    <input placeholder="Nuevo Departamento" name="Departamento" type={'text'} onChange={(e)=> handleInputOnChange(e)} />
+                </form>
 
             </div>
-            <form name="Departamento" onSubmit={(e)=> {handleOnEdit(e, 'Departamento')}}>
-                <input placeholder="Nuevo Departamento" name="Departamento" type={'text'} onChange={(e)=> handleInputOnChange(e)} />
-            </form>
+            <div>
+                <div>Ultima vez Actualizado: </div>
+                    <div>
+                        {selectedProduct && 'Día: ' + Number(selectedProduct['updatedAt']?.split('T')[0]?.split('-')[2])}
+                    </div>
+                    <div>
+                        { selectedProduct && 'Mes: ' + Number(selectedProduct['updatedAt']?.split('T')[0]?.split('-')[1])}
+                    </div>
+                    <div>
+                        { selectedProduct &&  'Año: ' + Number(selectedProduct['updatedAt']?.split('T')[0]?.split('-')[0])}
+                    </div>
+                <div>
+                    {todaysDate.getMonth()+1 > Number(selectedProduct['updatedAt']?.split('T')[0]?.split('-')[1]) ? 'Producto Desactualizado' : 'Producto Actualizado'   }
+                </div>
+            </div>
             <button onClick={() => {incrementCounter() }}>
                 +
             </button>
