9c3c182f27e99f2ae463048c9b0805bb87299736
Mettfan <cesaryannick6@gmail.com>
2022-10-17 23:48:52 -0500

    Solving product overlaping

--- api/src/Controllers/ProductControllers/postProduct.js
+++ api/src/Controllers/ProductControllers/postProduct.js
@@ -5,16 +5,23 @@ const { Product, User } = require("../../db.js");
 const postProduct = async (req, res, next) => {
 
   let { productos } = req.body
+  let found = null
   try{
-    // console.log(JSON.stringify(productos) );
-    Product.bulkCreate([...productos]).then( d => {
-      console.log('status: ' + JSON.stringify(d));
-      Product.findAll().then( (d2) => {
-        console.log(d2,  ' <-database');
-        res.status(200).send({productos: productos, db: d2})
-      })
+    productos.map( async producto => {
+      found = await Product.findOne({where: {['Código']: producto.Código}})
+      if (!found){
+        Product.bulkCreate([...[producto]]).then( d => {
+          console.log('status: ' + JSON.stringify(d));
+          Product.findAll().then( (d2) => {
+            console.log(d2,  ' <-database');
+            res.status(200).send({productos: productos, db: d2})
+          })
+    
+        })
 
-    })
+      }
+    } )
+    // console.log(JSON.stringify(productos) );
   }
   catch(error){
     res.send({error: error.error})
