cf56a091e03770a7691aa7aa818e584c64e6c861
Mettfan <cesaryannick6@gmail.com>
2023-06-02 23:40:45 -0600

    stop user from access stats

--- client/src/Components/TicketHandler/TicketHandler.js
+++ client/src/Components/TicketHandler/TicketHandler.js
@@ -2,12 +2,15 @@ import React from 'react';
 import Tickets from './Tickets/Tickets';
 import "./TicketHandler.css"
 import { useNavigate } from 'react-router';
+import { Cookie } from '@mui/icons-material';
 function TicketHandler() {
+    let cookie = new Cookie()
+    let user = cookie.get('user')
     let nav = useNavigate()
     return ( <>
         {/* <h1>Seleccionar Fecha de Tickets</h1> */}
         <div className='ticketFunctions'>
-            <button className='goToTicketsStats' onClick={() => nav('/tickets/stats')}>STATS</button>
+            {Number(user?.kyu) >= 9 && <button className='goToTicketsStats' onClick={() => nav('/tickets/stats')}>STATS</button>}
             {/* <input placeholder='ID' type={'text'} ></input> */}
             <Tickets></Tickets>
         </div>
