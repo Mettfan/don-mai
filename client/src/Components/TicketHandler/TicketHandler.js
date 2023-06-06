568ce04b320feeb72ca5f536932a5a2bff7d29e0
Mettfan <cesaryannick6@gmail.com>
2023-06-05 20:43:08 -0600

    solving bug on tickets

--- client/src/Components/TicketHandler/TicketHandler.js
+++ client/src/Components/TicketHandler/TicketHandler.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Tickets from './Tickets/Tickets';
 import "./TicketHandler.css"
 import { useNavigate } from 'react-router';
+import { useLocation } from 'react-router-dom';
 import Cookies from 'universal-cookie';
 
 function TicketHandler() {
     let cookie = new Cookies()
     let user = cookie.get('user')
     let nav = useNavigate()
+    let currentURL = window.location
+    useEffect(() => {
+        if(String(currentURL.href)?.slice(-1) === '?'){
+            nav('/tickets')
+        }
+        
+    }, [currentURL])
+    
     return ( <>
         {/* <h1>Seleccionar Fecha de Tickets</h1> */}
         <div className='ticketFunctions'>
