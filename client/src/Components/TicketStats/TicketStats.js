7cd779ab3d2bc26af33e4a5fd3a72bd712ec78bd
Mettfan <cesaryannick6@gmail.com>
2023-06-08 10:24:17 -0600

    add formatted stock download

--- client/src/Components/TicketStats/TicketStats.js
+++ client/src/Components/TicketStats/TicketStats.js
@@ -1,14 +1,29 @@
 import React, { useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import PrintComponent from '../TicketHandler/PrintComponent.js/PrintComponent';
 import GeneralStats from './GeneralStats/GeneralStats';
 import ProductCounter from './ProductCounter/ProductCounter';
 import './TicketStats.css'
+import { downloadExcel } from '../Convert/Convert';
+import { getMyProducts } from '../../features/products/productSlicetest';
+import Cookies from 'universal-cookie';
 function TicketStats() {
     let [productCounterEntry, setProductCounterEntry] = useState(0)
     let [productCounterOut, setProductCounterOut] = useState(0)
     let [ticketsType, setTicketsType] = useState('entry')
+    let userProducts = useSelector( state => state.products.userProducts)
+    let cookie = new Cookies()
+    let user = cookie.get('user')
+    let dispatch = useDispatch()
     
+    let getUserProducts = () => {
+        dispatch(getMyProducts({userId: user.id}))
+    }
+    useEffect(() => {
+        if(user){
+            getUserProducts()
+        }
+    }, [])
     // useEffect(() => {
     //     if(tickets){
     //         setProductCounter(calculateTotalQuantity(tickets, ticketsType)) 
@@ -50,6 +65,35 @@ function TicketStats() {
     let monthSelector = useState(3)
     let [selectedButton, setSelectedButton] = useState(null)
     let [comparedButton, setComparedButton] = useState(null)
+    function downloadStock(){
+        const current = new Date();
+        const date = `${current.getDate()}-${current.getMonth()+1}-${current.getFullYear()}`;
+        // No. / quantity / Producto / P. Calculado / Total 
+        let total = 0
+        let cleanStock = []
+        let sortedStock = []
+        sortedStock = [...userProducts]?.sort((x, y) => x?.Producto?.localeCompare(y?.Producto))
+        
+        let stockTemplate = sortedStock?.map((product) => {
+            // gUARDA P VENTA EN UNA VARIABLE Y RESTALE EL 10 PORCIENTO
+            let price = product['P. Venta']
+            let buyPrice = product['P. Compra']
+            let formattedBuyPrice = (!buyPrice?.includes('$')  ? Number(buyPrice) : Number(buyPrice.slice(1)))
+            let formattedPrice = price[0]  !== '$' ? Number(price) : Number(price.slice(1))
+            let calculatedPrice = formattedBuyPrice || (formattedPrice - (formattedPrice * 0.10 )) // Aqui se le cambia el porcentaje a calcular
+            let totalCalculated = (calculatedPrice* Number(product?.quantity) )
+            total += totalCalculated
+            if (product?.quantity !== null && product?.quantity > 0){
+                return { Codigo: product?.Código, Cantidad: product?.quantity, Producto: product?.Producto || '?', ["P. Venta"]: price || '?' , ['P. Calculado']: calculatedPrice  || '?', Total: totalCalculated, Acumulativo: total   }
+            }
+        })
+        cleanStock = stockTemplate.filter(product => product !== undefined)
+        cleanStock = cleanStock?.map((product, index) => {
+            return {N: index + 1, ...product}
+        })
+        console.log(cleanStock);
+        // downloadExcel(cleanStock, 'Inventario: ' + date )
+    }
     let printTickets = () => {
         return (<div  >
 
@@ -114,6 +158,7 @@ function TicketStats() {
         <button onClick={() => setPrintComponentPosition('03')} >1</button> */}
         {/* {JSON.stringify(tickets)} */}
         <PrintComponent buttonComponent= {<div>Imprimir Tickets</div>} component = {printTickets()}></PrintComponent>
+        <button onClick={() => {downloadStock()}}>DESCARGAR STOCK</button>
         <div className='ticketStatsContainer'>
 
             {tickets?.map(ticket => {
